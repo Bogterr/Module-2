39d41448e311fb87545c11dc3ea517a4790a614b
Bogdan <bog_berdsk@mail.ru>
2024-08-15 00:09:13 +0700

    добавлены комментарии

--- module_2_2.py
+++ module_2_2.py
@@ -3,49 +3,50 @@ import random                       # чтобы было интересно и 
 
 ######################          !!!GENERATOR!!!             ######################
 
-var_ = []
+var_ = []                                               # список для чисел
 
-# variable 1                                            1 вариант до int(1000)
+# variable 1                                            # 1 вариант до int(1000)
 for i in range(3):
-    random_num = int(random.randint(1, 1000))
-    var_.append(random_num)
+    random_num = int(random.randint(1, 1000))     # Генерируем числа
+    var_.append(random_num)                             # пишем в список
 # print(var_)
 
-first = var_[0]
+first = var_[0]                                         # присваеваем переменным из списка
 second = var_[1]
 third = var_[2]
 
-print("Первое значение = ", first)
+print("Первое значение = ", first)                      # Выводим пользователю
 print("Второе значение = ", second)
 print("Третье значение = ", third)
 
-if first == second and first == third:
+# УСЛОВИЯ
+if first == second and first == third:                                                      # если 3 из 3 равны, то
     print("Все три числа случайно оказались равны!!! Купи билетик :) Результат = ", 3)
-elif first == second or first == third or second == third:
+elif first == second or first == third or second == third:                                  # или если 2 из 3 равны, то
     print("Совпала пара! Результат = ", 2)
-else:
+else:                                                                                       # иначе если совпадений нет
     print("Совпадения нет, не переживай - все числа случайны! Результат = ", 0)
 print()
 ####################################################################################
-time.sleep(2)
+
+time.sleep(2)                                           # Ожидаем пару секунд
+
 ######################          РУЧНОЕ УПРАВЛЕНИЕ             ######################
 
 print("Давай теперь попробуем сами?")
 
-count = 0
+count = 0                                               # Вводим счетчик
 
-
-
-while count != 3:
+while count != 3:                                       # пока счетчик не = 3, то
     digit = False
     while digit == False:
-        first = input("Введи первое число = ")
-        if first.isdigit():
+        first = input("Введи первое число = ")          # Пользовательский ввод переменной
+        if first.isdigit():                             # Если 1 переменная - Число, то
             # print("Первое число = ", first)
             print()
-            digit = True
+            digit = True                                # Переходим ко 2й переменной
             count += 1
-        else:
+        else:                                           # Иначе если введенное значение НЕ Число!
             print("Введенное значение: ", first, " - не является числом, давай еще раз...")
             print()
 
